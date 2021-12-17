4fbb6294c6275795aa5052d99130200eb81610aa
dit <ditazanelli@gmail.com>
2021-12-17 15:50:49 -0600

    layout for quiz

--- assets/script.js
+++ assets/script.js
@@ -12,7 +12,7 @@ var buttons = ["$yellow-300", "$yellow-300", "$yellow-300", "$yellow-400"];
 
 function makeColorButtons(c) {
   for (var i = 0; i < c.length; i++) {
-    $('.butt-options').append('<button class=' + c[i] + '>' + "color" +
+    $('.butt-options').append('<button class=' + c[i] + '>' + "color  " +
       '</button>');
   }
 }
