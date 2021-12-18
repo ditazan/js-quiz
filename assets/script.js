ad6ed8ef16e6c2841474540b6f346068b470ed73
dit <ditazanelli@gmail.com>
2021-12-18 14:26:17 -0600

    timer created

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,38 @@
 
 
-//////// 
-var timer = $("#timer");
-console.log(timer.val());
 var quizArea = $("#quiz-area");
 var colorButtons = $(".butt-options");
 
 var colorButton = "";
 
-var buttons = ["$yellow-300", "$yellow-300", "$yellow-300", "$yellow-400"];
+var buttons = ['btn-dark', 'btn-primary', 'btn-primary', 'btn-primary'];
 
 function makeColorButtons(c) {
   for (var i = 0; i < c.length; i++) {
-    $('.butt-options').append('<button class=' + c[i] + '>' + "color  " +
-      '</button>');
+    $('.butt-options').append('<button class= "btn ' +c[i]+ '">  .......  </button>');
   }
 }
 
-makeColorButtons(buttons);
+
+var timerEl = $('#timer');
+
+function countdown() {
+    var timeLeft = 10;
+  
+    var timeInterval = setInterval(function () {
+      if (timeLeft > 0) {
+        timerEl.text('time : ' + timeLeft);
+        timeLeft--;
+      } else if (timeLeft === 0) {
+        timerEl.text('time : ' + timeLeft);
+        clearInterval(timeInterval);
+     
+        
+      }
+    }, 1000);
+  }
+
+  countdown();
+  makeColorButtons(buttons);
+
+  console.log(timerEl);
