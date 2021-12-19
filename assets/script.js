aa7ee5a59d4a33b3c8c39f4b63384ed79dc9b3ef
dit <ditazanelli@gmail.com>
2021-12-19 13:09:04 -0600

    beg of quiz and quiz end:

--- assets/script.js
+++ assets/script.js
@@ -1,39 +1,62 @@
 var quizArea = $("#quiz-area");
 var colorButtons = $(".butt-options");
+// var score = "";
 
 var colorButton = "";
 
-var buttons = ['btn-dark', 'btn-primary', 'btn-primary', 'btn-primary'];
+const randomColor = Math.floor(Math.random()*16777215).toString(16);
+
+var buttons = ["wrong", "wrong", "wrong", "right"];
+
 
 function makeColorButtons(c) {
   for (var i = 0; i < c.length; i++) {
-    $('.butt-options').append('<button class= "btn ' +c[i]+ '">  .......  </button>');
+    $(".butt-options").append(
+      '<button class= "btn color-btn ' + c[i] + '">  .......  </button>'
+      
+    );
   }
 }
 
-
-var timerEl = $('#timer');
+var timerEl = $("#timer");
 
 function countdown() {
-    var timeLeft = 10;
-  
-    var timeInterval = setInterval(function () {
-      if (timeLeft > 0) {
-        timerEl.text('time : ' + timeLeft);
-        timeLeft--;
-      } else if (timeLeft === 0) {
-        timerEl.text('time : ' + timeLeft);
-        clearInterval(timeInterval);
-      }
-    }, 1000);
-  }
+  var timeLeft = 10;
+  var scoreBtnEl = "";
 
-  countdown();
-
- function start(){
-     var startPrompt = $("<p id= 'start-prompt' class='text-light text-center'> Test your eyes by selecting the odd color out. </p>");
-     startPrompt.insertAfter("h1");
-     var startBtn = $("<button id ='start-btn' class='btn btn-warning'> Start </button>")
-    startBtn.appendTo(".butt-options");
+  var timeInterval = setInterval(function () {
+    if (timeLeft > 0) {
+      timerEl.text("time : " + timeLeft);
+      timeLeft--;
+    } else if (timeLeft === 0) {
+      timerEl.text("time : " + timeLeft);
+      clearInterval(timeInterval);
+      $("h1").text("Times up !");
+      $("p").text("Your scored a : " );
+      
     }
- start();
+    
+  }, 1000);
+}
+
+
+
+function start() {
+  var startPrompt = $(
+    "<p id= 'start-prompt' class='text-light text-center'> Test your eyes by selecting the odd color out. </p>"
+  );
+  startPrompt.insertAfter("h1");
+  var startBtn = $(
+    "<button id ='start-btn' class='btn btn-warning'> Start </button>"
+  );
+  startBtn.appendTo(".butt-options");
+}
+
+start();
+
+$(".butt-options").on("click", "#start-btn", function () {
+$("#start-btn").remove();
+  $("#start-prompt").remove();
+  makeColorButtons(buttons);
+  countdown();
+});
